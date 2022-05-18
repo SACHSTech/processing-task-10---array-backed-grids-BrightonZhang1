e4c8507dd55472f897409facd0984e60896fa06a
Brighton Zhang <75390931+BrightonZhang1@users.noreply.github.com>
2022-05-18 13:53:54 +0000

    test

--- Sketch.java
+++ Sketch.java
@@ -1,13 +1,22 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
+	int intCellWidth = 20;
+  int intCellHeight = 20;
+  int intMargin = 5;
+  int intRowCount = 10;
+  int intColumnCount = 10;
+  int intCounter = 0;
+  int intScreenWidth = intColumnCount * intCellWidth + (intMargin * (intColumnCount + 1)); 
+  int intScreenHeight = intRowCount * intCellHeight + (intMargin * (intRowCount + 1));
+  int intGrid [][] = new int [10][10];
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
     // put your size call here
-    size(500, 600);
+    size(intScreenWidth, intScreenHeight);
   }
 
   /** 
@@ -15,17 +24,45 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(255,255,255);
+    background(0);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
+    for(int row = 0; row < intRowCount; row++){
+      for(int column = 0; column < intColumnCount; column++ ){
+        if(intGrid[row][column] == 1){
+          fill(0,255,0);
+          rect(column * (intCellWidth + intMargin) + intMargin, row * (intCellHeight + intMargin) + intMargin, intCellWidth, intCellHeight);
+        }
+        
+        else{
+          fill(255);
+          rect(column * (intCellWidth + intMargin) + intMargin, row * (intCellHeight + intMargin) + intMargin, intCellWidth, intCellHeight);
+        }
+      }
+    }
   }
 
-  public void mousePressed(){
-    
+  /** 
+   * This method, mousePressed(), checks the mouse coordinate and grid coordinate, using it to light up a cell to green if pressed o in the draw() method.
+   */
+  public void mousePressed() {
+    System.out.println("mouse coordinates: (" + mouseX + ", " + mouseY + "); ");
+    for(int row = 0; row < intRowCount; row++){
+      for(int column = 0; column < intColumnCount; column++ ){
+        if(mouseX > column * (intCellHeight + intMargin) + intMargin && mouseX < column * (intCellHeight + intMargin) + intMargin + 20 && mouseY > row * (intCellWidth + intMargin) + intMargin && mouseY < row * (intCellWidth + intMargin) + intMargin + 20){
+          System.out.println("grid coordinates: (row: " + (row + 1) + ", column: " + (column + 1) + ")");
+          if(intGrid[row][column] == 1){
+            intGrid[row][column] = 0;
+          }
+          else if(intGrid[row][column] == 0){
+            intGrid[row][column] = 1;
+          }
+        }
+      }
+    }
   }
 }
